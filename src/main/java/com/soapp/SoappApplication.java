7e8aefd0951c91028c7871a4ec3a0d2b8af94c8e
joxinalde <joxinalde@csjn.gov.ar>
2024-02-07 10:30:12 -0300

    inicio proyecto soap

---
+++ src/main/java/com/soapp/SoappApplication.java
@@ -0,0 +1,13 @@
+package com.soapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/soapp/SoappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
