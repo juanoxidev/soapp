4af9cd6322b00116c892022714ff4f97979a8087
juanoxidev <123821231+juanoxidev@users.noreply.github.com>
2024-02-16 10:11:23 -0300

    PaisRepository.java modificado para aceptar .json

--- src/main/java/com/soapp/repository/PaisRepository.java
+++ src/main/java/com/soapp/repository/PaisRepository.java
@@ -1,52 +1,47 @@
 package com.soapp.repository;
 
-import java.util.HashMap;
-import java.util.Map;
-
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.annotation.PostConstruct;
+import localhost._8081.ws.Pais;
 import org.springframework.stereotype.Component;
 import org.springframework.util.Assert;
-
-import jakarta.annotation.PostConstruct;
-import localhost._8081.ws.Moneda;
-import localhost._8081.ws.Pais;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Component
 public class PaisRepository {
- private static final Map<String, Pais> paises = new HashMap<>();
- 
- @PostConstruct
- public void initData() {
-	 Pais argentina = new Pais();
-	 argentina.setNombre("argentina");
-	 argentina.setCapital("Ciudad Autonoma de Buenos Aires");
-	 argentina.setMoneda(Moneda.ARS);
-	 argentina.setPoblacion(46234830);
-	 argentina.setBandera("https://flagcdn.com/w2560/ar.png");
-	 
-	 paises.put(argentina.getNombre(), argentina);
-	 
-	 Pais colombia = new Pais();
-	 colombia.setNombre("colombia");
-	 colombia.setCapital("Bogota");
-	 colombia.setMoneda(Moneda.COP);
-	 colombia.setPoblacion(52210913);
-	 colombia.setBandera("https://flagcdn.com/w2560/co.png");
-	 
-	 
-	 paises.put(colombia.getNombre(), colombia);
-	 
-	 Pais ecuador = new Pais();
-	 ecuador.setNombre("ecuador");
-	 ecuador.setCapital("Quito");
-	 ecuador.setMoneda(Moneda.USD);
-	 ecuador.setPoblacion(1775700);
-	 ecuador.setBandera("https://flagcdn.com/w2560/ec.png");
-	 
-	 paises.put(ecuador.getNombre(), ecuador);
- }
- 
- public Pais encontrarPais (String name) {
-	 Assert.notNull(name, "El nombre del pais no puede estar vacio");
-	 return paises.get(name.toLowerCase());
- }
+
+    private static final Map<String, Pais> paises = new HashMap<>();
+
+    @PostConstruct
+    public void initData() {
+        cargarPaisesDelJson();
+    }
+
+    public Pais encontrarPais (String name) {
+        Assert.notNull(name, "El nombre del pais no puede estar vacio");
+        return paises.get(name.toLowerCase());
+    }
+
+    private void cargarPaisesDelJson(){
+        ObjectMapper objectMapper = new ObjectMapper();
+        TypeReference<List<Pais>> typeReference = new TypeReference<>() {};
+
+        try (InputStream inputStream = getClass().getClassLoader().getResourceAsStream("paises.json")){
+            List<Pais> paisesList = objectMapper.readValue(inputStream, typeReference);
+
+            for (Pais pais: paisesList) {
+                Assert.notNull(pais.getNombre(), "El valor del nombre del pais no puede estar vacio");
+                paises.put(pais.getNombre(), pais);
+            }
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
